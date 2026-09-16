2f5445abc0e9f48311b38e277f28f0899607d179
nba1985 <nicolasbertorello@gmail.com>
2026-09-16 17:50:42 -0300

    correcciones al back 002

--- back/src/controllers/canchas.controller.js
+++ back/src/controllers/canchas.controller.js
@@ -1,2 +1,21 @@
-// TODO: implementar el controlador que lista las canchas mediante el procedimiento almacenado.
+const { pool, poolConnect } = require("../config/db");
 
+const listarCanchas = async (req, res) => {
+  try {
+    await poolConnect;
+
+    const result = await pool
+      .request()
+      .execute("usp_ListarCanchas");
+
+    res.status(200).json(result.recordset);
+  } catch (error) {
+    res.status(500).json({
+      mensaje: "Error al listar las canchas"
+    });
+  }
+};
+
+module.exports = {
+  listarCanchas
+};
